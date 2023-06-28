8f30e8613fdc1dc947bdf1754b86328e4344a776
Aziz Baubaid <3zuccino@gmail.com>
2023-06-28 21:39:23 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Aziz Baubaid on 28.06.23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
